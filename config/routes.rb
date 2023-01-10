19720dc4e67c67c1522caf0fdc8117b0ab7d62f5
sandeep saini <sandeepsaini14u@gmail.com>
2023-01-10 15:29:38 +0530

    adding_navbar

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+root "home#index"
+get "/home", to: "home#index"
+ 
+end
