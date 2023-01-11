6b70240df12d3fdcf8ea9fd19753eb39e86e7326
sandeep saini <sandeepsaini14u@gmail.com>
2023-01-11 18:17:22 +0530

    creating front page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root "home#index"
 get "/home", to: "home#index"
+get "/index", to: "home#index"
+get "/Catagory", to: "home#Catagory"
+get "/contact", to: "home#contact"
  
 end
