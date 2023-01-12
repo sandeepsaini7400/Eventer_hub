7a937e155897a4639f4e3f244e876ba382a43c5f
sandeep saini <sandeepsaini14u@gmail.com>
2023-01-12 18:25:27 +0530

    adding pages and models

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ get "/home", to: "home#index"
 get "/index", to: "home#index"
 get "/Catagory", to: "home#Catagory"
 get "/contact", to: "home#contact"
+get "/eventer" , to: "home#eventer"
+get "/Register", to: "home#Register"
+get "/login", to: "home#login"
  
 end
